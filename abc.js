ba7959dc53f46c4c4fd5633b2314057a97ca657e
bharath-krishnan2145 <bharathkrishnan.jbk@gmail.com>
2024-09-04 18:38:40 +0530

    first commit

---
+++ abc.js
@@ -0,0 +1,15 @@
+function iphone(){
+    console.log(" this is iPhone")
+   
+
+}
+
+
+function redmi(){
+    console.log("this is redmi")
+}
+function oppo(){
+    console.log("this is oppo")
+}
+oppo()
+iphone()
